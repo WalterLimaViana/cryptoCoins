3520d79c199d06a0654bf2bec4d98a69b0fce127
WalterLimaViana <walter.lima.viana@gmail.com>
2022-05-13 20:50:45 -0300

    mode hive created on settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:cryptocoins/configs/app_settings.dart';
+import 'package:cryptocoins/configs/hive_config.dart';
 import 'package:cryptocoins/meu_aplicativo.dart';
 import 'package:cryptocoins/repositories/favoritas_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HiveConfig.start();
   runApp(
     MultiProvider(
       providers: [
