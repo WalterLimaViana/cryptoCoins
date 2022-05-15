fcf4153c326180a64d566a20251a621766ba997b
WalterLimaViana <walter.lima.viana@gmail.com>
2022-05-15 20:46:00 -0300

    created method compra on moedas_detalhes_page

--- lib/pages/moedas_detalhes_page.dart
+++ lib/pages/moedas_detalhes_page.dart
@@ -1,7 +1,10 @@
+import 'package:cryptocoins/configs/app_settings.dart';
 import 'package:cryptocoins/models/moeda.dart';
+import 'package:cryptocoins/repositories/conta_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class MoedasDetalhesPage extends StatefulWidget {
   Moeda moeda;
@@ -12,13 +15,17 @@ class MoedasDetalhesPage extends StatefulWidget {
 }
 
 class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
-  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  late NumberFormat real;
   final _form = GlobalKey<FormState>();
   final _valor = TextEditingController();
   double quantidade = 0;
-  comprar() {
+  late ContaRepository conta;
+
+  comprar() async {
     if (_form.currentState!.validate()) {
       //salvar a compra
+      await conta.comprar(widget.moeda, double.parse(_valor.text));
+
       Navigator.pop(context);
 
       ScaffoldMessenger.of(context).showSnackBar(
@@ -28,6 +35,8 @@ class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
 
   @override
   Widget build(BuildContext context) {
+    readNumberFormat();
+    conta = Provider.of<ContaRepository>(context, listen: false);
     return Scaffold(
         appBar: AppBar(
           title: Center(child: Text(widget.moeda.name)),
@@ -98,6 +107,8 @@ class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
                         return 'Informe o valor da compra';
                       } else if (double.parse(value) < 50) {
                         return 'Compra minima é R\$50,00';
+                      } else if (double.parse(value) > conta.saldo) {
+                        return 'Você não possui saldo suficiente!';
                       }
                       return null;
                     },
@@ -129,5 +140,10 @@ class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
             ],
           ),
         ));
+  }
+
+  readNumberFormat() {
+    final loc = context.watch<AppSettings>().locale;
+    real = NumberFormat.currency(locale: loc['locale'], name: loc['name']);
   }
 }
--- lib/repositories/conta_repository.dart
+++ lib/repositories/conta_repository.dart
@@ -1,5 +1,7 @@
 import 'package:cryptocoins/database/db.dart';
+import 'package:cryptocoins/models/moeda.dart';
 import 'package:cryptocoins/models/posicao.dart';
+import 'package:cryptocoins/repositories/moeda_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -16,6 +18,7 @@ class ContaRepository extends ChangeNotifier {
 
   _initRepository() async {
     await _getSaldo();
+    await _getCarteira();
   }
 
 // pegar o saldo da conta
@@ -33,6 +36,63 @@ class ContaRepository extends ChangeNotifier {
       'saldo': valor,
     });
     _saldo = valor;
+    notifyListeners();
+  }
+
+  //método comprar:
+  comprar(Moeda moeda, double valor) async {
+    db = await DB.instance.database;
+    await db.transaction((txn) async {
+      //Verificar se a moeda já foi comprada
+      final posicaoMoeda = await txn.query(
+        'carteira',
+        where: 'sigla = ?',
+        whereArgs: [moeda.sigla],
+      );
+      //se não tem a moeda em carteira
+      if (posicaoMoeda.isEmpty) {
+        await txn.insert('carteira', {
+          'sigla': moeda.sigla,
+          'moeda': moeda.name,
+          'quantidade': (valor / moeda.preco).toString()
+        });
+      } //Já tem a moeda em carteira
+      else {
+        final atual = double.parse(posicaoMoeda.first['quantidade'].toString());
+        await txn.update('carteira',
+            {'quantidade': (atual + (valor / moeda.preco)).toString()},
+            where: 'sigla = ?', whereArgs: [moeda.sigla]);
+      }
+
+      //Inserir a compra no historico
+      await txn.insert('historico', {
+        'sigla': moeda.sigla,
+        'moeda': moeda.name,
+        'quantidade': (valor / moeda.preco).toString(),
+        'valor': valor,
+        'tipo_operacao': 'compra',
+        'data_operacao': DateTime.now().millisecondsSinceEpoch
+      });
+
+      //Atualizar o saldo
+      await txn.update('conta', {'saldo': saldo - valor});
+    });
+    await _initRepository();
+    notifyListeners();
+  }
+
+  _getCarteira() async {
+    _carteira = [];
+    List posicoes = await db.query('carteira');
+    posicoes.forEach((posicao) {
+      Moeda moeda = MoedaRepository.tabela.firstWhere(
+        (m) => m.sigla == posicao['sigla'],
+      );
+      _carteira.add(Posicao(
+        moeda: moeda,
+        quantidade: double.parse(posicao['quantidade']),
+      ));
+    });
     notifyListeners();
   }
 }
